6a03e6c2a77de0c926e97e9f83b1da7ff6779c6e
aethe-ryx <111909930+aethe-ryx@users.noreply.github.com>
2022-08-31 13:29:02 -0500

    Update adding_numbers.py

--- adding_numbers.py
+++ adding_numbers.py
@@ -1,1 +1,12 @@
-
+def add(n1, n2):
+	if n1 == "" or n1 is None:
+		return("Invalid Operation")
+	if n2 == "" or n2 is None:
+		return("Invalid Operation")
+		
+	n1 = int(n1)
+	n2 = int(n2)
+	
+	answer = n1 + n2
+	answer = str(answer)
+	return answer
