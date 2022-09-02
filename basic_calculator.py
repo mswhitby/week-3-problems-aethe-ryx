a10cc852ebc116d1a6259d539779762b415770a8
aethe-ryx <111909930+aethe-ryx@users.noreply.github.com>
2022-09-02 12:48:34 -0500

    Update basic_calculator.py

--- basic_calculator.py
+++ basic_calculator.py
@@ -1,1 +1,11 @@
-
+def calculator(num1, operator, num2):
+	if operator == '/' and num2 == 0:
+		return "Can't divide by 0!"
+	if operator == '+':
+		return num1 + num2
+	if operator == '-':
+		return num1 - num2
+	if operator == '/':
+		return num1 / num2
+	if operator == '*':
+		return num1 * num2
